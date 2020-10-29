7cb0cc03b6af6401b56f25655f74fcfd085a5455
foo <sandwrm@pm.me>
2020-10-29 18:57:13 +0530

    implement MovieService

---
+++ src/main/java/org/eldi/movietracker/movie/MovieServiceImpl.java
@@ -0,0 +1,11 @@
+package org.eldi.movietracker.movie;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MovieServiceImpl implements MovieService{
+
+    public Optional<List<Movie>> search(String searchQuery) {
+        return Optional.empty();
+    }
+}
