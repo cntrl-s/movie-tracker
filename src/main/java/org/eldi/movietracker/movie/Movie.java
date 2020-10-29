76cc9dfa95e159cec3d015f597b67071d43da9c1
foo <sandwrm@pm.me>
2020-10-29 14:29:38 +0530

    add properties

--- src/main/java/org/eldi/movietracker/movie/Movie.java
+++ src/main/java/org/eldi/movietracker/movie/Movie.java
@@ -1,4 +1,28 @@
 package org.eldi.movietracker.movie;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.net.URL;
+
 public class Movie {
+    private String title;
+    private String year;
+    private String rated;
+    private String released;
+    private int runtime;
+    private String genre;
+    private String director;
+    private String writer;
+    private String actors;
+    private String plot;
+    private String language;
+    private String country;
+    private String awards;
+    private URL posterUrl;
+    // ratings
+    private int metaScore;
+    private double imdbRating;
+    private long imdbVotes;
+    private String imdbID;
+    private Type type;
 }
