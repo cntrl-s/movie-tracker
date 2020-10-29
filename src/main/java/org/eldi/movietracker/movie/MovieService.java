14ba650c2af503a9e6b1e7e49a2154116d81b6e5
foo <sandwrm@pm.me>
2020-10-29 18:56:20 +0530

    define method

--- src/main/java/org/eldi/movietracker/movie/MovieService.java
+++ src/main/java/org/eldi/movietracker/movie/MovieService.java
@@ -1,4 +1,8 @@
 package org.eldi.movietracker.movie;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MovieService {
+    public Optional<List<Movie>> search(String searchQuery);
 }
