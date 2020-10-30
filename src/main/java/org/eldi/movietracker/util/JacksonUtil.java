031ed84ea16aaab7c803d9d830a2909ada51c007
foo <sandwrm@pm.me>
2020-10-30 14:13:28 +0530

    rename variable

--- src/main/java/org/eldi/movietracker/util/JacksonUtil.java
+++ src/main/java/org/eldi/movietracker/util/JacksonUtil.java
@@ -3,7 +3,10 @@ package org.eldi.movietracker.util;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class JacksonUtil {
-    public static final ObjectMapper objectMapper = new ObjectMapper()
+    public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
             .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
 }
