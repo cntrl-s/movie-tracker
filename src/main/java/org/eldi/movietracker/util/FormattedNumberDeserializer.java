d3105d1d4a70b7600e6b879af59322a6267448dd
foo <sandwrm@pm.me>
2020-11-01 20:33:59 +0530

    custom deserializer for formatted numbers

---
+++ src/main/java/org/eldi/movietracker/util/FormattedNumberDeserializer.java
@@ -0,0 +1,20 @@
+package org.eldi.movietracker.util;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+
+public class FormattedNumberDeserializer extends JsonDeserializer<Long> {
+    public Long deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+        JsonNode jsonNode = jsonParser.getCodec().readTree(jsonParser);
+        String imdbVotes = jsonNode.asText();
+        imdbVotes = imdbVotes.replaceAll(",", "");
+        return Long.valueOf(imdbVotes);
+    }
+}
