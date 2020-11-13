3ffd1591cd53eeed546698e7801f2caa431fe0f0
not-the-avatar <sandwrm@pm.me>
2020-11-13 19:32:53 +0530

    add formatQuery()

--- src/main/java/org/eldi/movietracker/util/APIQueries.java
+++ src/main/java/org/eldi/movietracker/util/APIQueries.java
@@ -1,12 +1,13 @@
 package org.eldi.movietracker.util;
 
 public class APIQueries {
-    public static final String BASE_URL = "https://www.omdbapi.com/";
+    public static final String BASE_URL = "https://www.omdbapi.com";
+    public static final String API_KEY = "&apikey=";
 
     public static String getSearchQuery(String searchQuery) {
         // format query
-        String formattedSearchQuery = "";
-        return BASE_URL + "/?s=" + formattedSearchQuery;// + apikey
+        String formattedSearchQuery = formatQuery(searchQuery);
+        return BASE_URL + "/?s=" + formattedSearchQuery + API_KEY;
     }
 
     public static String getImdbSearchQuery(String imdbID) {
@@ -19,5 +20,9 @@ public class APIQueries {
         // format query
         String formattedSearchQuery = "";
         return BASE_URL + "/?t=" + formattedSearchQuery;
+    }
+
+    private static String formatQuery(String query) {
+        return query.replaceAll(" ", "+");
     }
 }
