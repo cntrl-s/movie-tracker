942cc5830d819cbdc377c36cd3669bbd742a7370
not-the-avatar <sandwrm@pm.me>
2020-11-19 18:40:56 +0530

    reformat queries

--- src/main/java/org/eldi/movietracker/util/APIQueries.java
+++ src/main/java/org/eldi/movietracker/util/APIQueries.java
@@ -1,25 +1,26 @@
 package org.eldi.movietracker.util;
 
 public class APIQueries {
-    private static final String BASE_URL = "https://www.omdbapi.com";
-    private static final String API_KEY = "&apikey=";
+    private static final String BASE_URL = "https://www.omdbapi.com/";
+    private static final String API_PARAMETER = "&apikey=";
 
     public static String getSearchQuery(String searchQuery) {
-        // format query
         String formattedSearchQuery = formatQuery(searchQuery);
-        return BASE_URL + "/?s=" + formattedSearchQuery + API_KEY + UserAPIKeyPersistUtil.getUserAPIKey();
+        return BASE_URL + "?s=" + formattedSearchQuery + getApiParameterWithKey();
     }
 
     public static String getImdbSearchQuery(String imdbID) {
-        // format query
-        String formattedSearchQuery = "";
-        return BASE_URL + "/?i=" + formattedSearchQuery;
+        return BASE_URL + "?i=" + imdbID + getApiParameterWithKey();
     }
 
     public static String getTitleSearchQuery(String title) {
         // format query
         String formattedSearchQuery = "";
-        return BASE_URL + "/?t=" + formattedSearchQuery;
+        return BASE_URL + "?t=" + formattedSearchQuery;
+    }
+
+    private static String getApiParameterWithKey() {
+        return API_PARAMETER + UserAPIKeyPersistUtil.getUserAPIKey();
     }
 
     private static String formatQuery(String query) {
